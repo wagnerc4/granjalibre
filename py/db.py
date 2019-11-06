f4b2c15e29a07adcf40d050bc22211768bf4139a
wagner <wagnerc4@gmail.com>
2019-11-06 10:35:57 -0600

    Update db.py

--- py/db.py
+++ py/db.py
@@ -1,16 +1,13 @@
 from psycopg2 import connect
-# from sqlite3 import connect
-from json import dumps, JSONEncoder
+from json import JSONEncoder, dumps
 from decimal import Decimal
 from datetime import date
-#from subprocess import call
 
 
 class jsonEncoder(JSONEncoder):
   def default(self, obj):
     if isinstance(obj, Decimal): return float(obj)
     elif isinstance(obj, date): return str(obj)
-    # return super(jsonEncoder, self).default(obj)
     return JSONEncoder.default(self, obj)
 
 
@@ -20,7 +17,6 @@ class dbObj:
   def __init__(self, privilege):
     try:
       self.conn = connect("dbname='granjalibre' user='%s_user'" % privilege)
-      # self.conn = connect(dbname)
       self.cur = self.conn.cursor()
     except Exception as e:
       raise Exception("db conn error: " + str(e))
@@ -41,26 +37,14 @@ class dbObj:
     return self.cur.fetchall()
   def getRowsAssoc(self, sql, args=None):
     self.execute(sql, args)
-    cols, tmp = self.cur.description, self.cur.fetchall()
-    cols_len = len(cols)
-    #return [{cols[i][0]:row[i] for i in range(cols_len)} for row in tmp]
-    rows = []
-    for line in tmp:
-      row = {}
-      for i in range(0, cols_len, 1): row[cols[i][0]] = line[i]
-      rows.append(row)
-    return rows
+    cols, rows = self.cur.description, self.cur.fetchall()
+    return [{cols[i][0]:row[i] for i in range(len(cols))} for row in rows]
   def getDescription(self):
     return [cn[0] for cn in self.cur.description]
   def __del__(self):
     self.conn.commit()
     self.cur.close()
     self.conn.close()
-
-
-#def create_activity(id):
-#  call("psql granjalibre -v activity='%i' -f %s" % (id, 'py/db_reproduction.sql'), shell=True)
-#  call("psql granjalibre -v activity='%i' -f %s" % (id, 'py/db_production.sql'), shell=True)
 
 
 def wrapper(func, rq, dump=False):
