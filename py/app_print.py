7f5503d432f95d564396e2e6ef0fb8af690087c4
wagner <wagnerc4@gmail.com>
2019-11-06 10:26:26 -0600

    Update app_print.py

--- py/app_print.py
+++ py/app_print.py
@@ -2,18 +2,18 @@ from re import sub
 from json import loads
 from datetime import datetime, timedelta
 from pytz import timezone
+
 from xhtml2pdf import pisa
 from io import StringIO, BytesIO
 
-#from email.mime.application import MIMEApplication
-#from email.mime.multipart import MIMEMultipart
-#from email.mime.text import MIMEText
-#from smtplib import SMTP
+# from email.mime.application import MIMEApplication
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+# from smtplib import SMTP
 
+from py.app_resumens import select_query_saved
+from py.utilities import unescape
 from py.app_forms_reproduction import repro_resumen
-from py.app_settings import select_query_saved
-from py.utilities import unescape, thous
-from py.utilities_js import context
 
 
 cr_tz = timezone('America/Costa_Rica')  # datetime.now(cr_tz)
@@ -22,6 +22,7 @@ cr_tz = timezone('America/Costa_Rica')  # datetime.now(cr_tz)
 def set_pdf(header, content):
   html = '<html><head>' + \
          '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />' + \
+         '<style>.table {font-size:125%;}</style>' + \
          '<style>.table {border-collapse: collapse; border-style: hidden;}</style>' + \
          '<style>.table th {border-top:1px solid #ccc; border-bottom:1px solid #ccc;}</style>' + \
          '<style>.table th, .table td {text-align:right; vertical-align:top;}</style>' + \
@@ -31,9 +32,9 @@ def set_pdf(header, content):
          content + \
          "Fecha (hora): %s" % datetime.now(cr_tz).strftime("%Y-%m-%d (%H:%M:%S)") + \
          '</body></html>'
-  #resultFile = open('/home/wagner/doc.pdf', 'w+b')
-  #pisaStatus = pisa.CreatePDF(html, dest=resultFile)
-  #resultFile.close()
+  # resultFile = open('/home/wagner/doc.pdf', 'w+b')
+  # pisaStatus = pisa.CreatePDF(html, dest=resultFile)
+  # resultFile.close()
   pdf = BytesIO()
   status = pisa.CreatePDF(StringIO(html), dest=pdf)
   if status.err:
@@ -47,37 +48,49 @@ def print_header(dbObj, rq):
   header = '<table><tr><td style="width:550px; vertical-align:middle;">' + \
            '<h1 style="font-size:300%">Granja Libre - ' + unescape(activity) + '</h1>' + \
            '</td><td style="width:150px;">' + \
-           '<img src="../logos/vaca.png" />' + \
+           '<img src="logos/vaca.png" />' + \
            '</td></tr></table>'
   return [header]
 
 
 def print_resumen(dbObj, rq):
   data = select_query_saved(dbObj, rq)
-  defs = loads(data['defs'])
-  table = "<table class='table table-striped' style='font-size:150%'>"
-  table += "  <caption><h2>%s</h2>%s%s</caption>" % (data['title'],
-                                                       '-' + rq['d1'] if 'd1' in rq else '',
-                                                       ',' + rq['d2'] if 'd2' in rq else '')
-  table += "  <thead>"
-  table += "    <tr><th>%s</th></tr>" % '</th><th>'.join([d['head'] for d in defs])
-  table += "  </thead>"
-  table += "  <tbody>"
-  # last=[[]], sums=[], mins=[], maxs=[]
-  for row in data['rows']:
-    tmp = []
-    for i in range(len(defs)):
-      tmp.append(context.eval(defs[i]['value'])(row) if 'value' in defs[i] else row[i])
-      tmp[i] = thous(tmp[i]) if isinstance(tmp[i], float) else str(tmp[i])
-    table += "  <tr><td>%s</td></tr>" % '</td><td>'.join(tmp)
-  table += "  </tbody>"
-  # table += "  <tfoot></tfoot>"
-  table += "</table>"
-  return [table]
+  html = '<span style="font-size:200%;">' + \
+         '<h2 style="display:inline;;">' + data['title'] + '</h2>' + \
+         ('-' + rq['d1'] if 'd1' in rq else '') + \
+         (',' + rq['d2'] if 'd2' in rq else '') + \
+         '</span>'
+  width = 0
+  for c in data['content']:
+    d = c['data']
+    if c['type'] in ('table', 'crosstable'):
+      html += '</tr></table>' if width > 0 else ''
+      html += '<table class="table table-striped">'
+      html += '<thead><tr><th>%s</th></tr></thead>' % '</th><th>'.join(d['head'])
+      html += '<tbody>'
+      html += ''.join(['<tr><td>%s</td></tr>' % '</td><td>'.join(map(str, b)) for b in d['body']])
+      html += '</tbody>'
+      if len(d['foot']) > 0:
+        html += '<tfoot><tr><th>%s</th></tr></tfoot>' % '</th><th>'.join(d['foot'][0])
+      html += '</table>'
+      width = 0
+    elif c['type'] == 'pivot':
+      html += '<table><tr>' if width == 0 else ''
+      html += '<td>%s</td>' % d['table']
+      html += '</tr></table>' if (width + d['width']) > 400 else ''
+      width = 0 if (width + d['width']) > 400 else width + d['width']
+    elif c['type'] == 'graph':
+      html += '<table><tr>' if width == 0 else ''
+      html += '<td><img src="%s" /></td>' % d['graph']
+      html += '</tr></table>' if (width + d['width']) > 400 else ''
+      width = 0 if (width + d['width']) > 400 else width + d['width']
+  html += '</table>'
+  return [html]
 
 
-#def print_pdf(dbObj, rq):
-#  return set_pdf(print_header(dbObj, rq)[0], print_resumen(dbObj, rq)[0])
+def print_pdf(dbObj, rq):
+  rq['pdf'] = True
+  return set_pdf(print_header(dbObj, rq)[0], print_resumen(dbObj, rq)[0])
 
 
 #def print_email(dbObj, rq):
